4cecdbed3508b6a77512709d7b0b783e0ac99dd2
Francesco Delfino <delfinof@gmail.com>
2011-07-07 12:52:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+###############
+OVERVIEW
+###############
+
+This SDK allows you to use the Lyrics Plugin App (currently in beta) from within your own application.
+If you have built a music application, you can just link the sdk as explained here and, when you want to display the lyrics, just make a call to our plugin through the connector class. 
+
+###############
+USAGE
+###############
+
+The integration steps are minimal:
+
+- integrate the lyrics button as in the "musiXmatchSDKActivity.java"
+
+- enable the testMode while integrating your application
+
+- accept musiXmatch terms of service (registering as a developer and notifying us your intention of use)
+
+- wait for the approval before publishing or distributing the application in any form
+
+- disable the testMode before publishing it
+
+###############
+FEEDBACK
+###############
+
+Please use the support@musixmatch.com email address for providing feedback about the sdk.
---
+++ src/com/musixmatch/lyrics/example/musiXmatchSDKActivity.java
@@ -0,0 +1,55 @@
+package com.musixmatch.lyrics.example;
+
+import com.musixmatch.lyrics.musiXmatchLyricsConnector;
+import com.musixmatch.lyrics.example.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class musiXmatchSDKActivity extends Activity {
+	private musiXmatchLyricsConnector lyricsPlugin= null;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final String mArtistName = "The Beatles";
+        final String mTrackName  = "Let It Be";
+        lyricsPlugin = new musiXmatchLyricsConnector(this);
+        
+        // Please uncomment this line when approved and before going in production
+        //
+        lyricsPlugin.setTestMode(false);
+
+        Button btn = (Button) findViewById(R.id.showLyrics);
+		btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (lyricsPlugin.getIsBound())
+					try {
+						lyricsPlugin.startLyricsActivity(mArtistName,mTrackName);
+					} catch (RemoteException e) {
+						lyricsPlugin.downloadLyricsPlugin();
+					}
+				else
+					lyricsPlugin.downloadLyricsPlugin();
+			}
+		});
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	if (!lyricsPlugin.getIsBound())
+    		lyricsPlugin.doBindService();
+
+    	super.onResume();
+    }
+
+}
