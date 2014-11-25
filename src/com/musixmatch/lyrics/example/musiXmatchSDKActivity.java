0e8b7cdcac7bc5e693e3b6c46d67b86838616f82
Mattia Maestrini <mattia.maestrini@musixmatch.com>
2014-11-25 11:16:03 +0100

    Updated musiXmatch connector to support Android Lollipop

--- src/com/musixmatch/lyrics/example/musiXmatchSDKActivity.java
+++ src/com/musixmatch/lyrics/example/musiXmatchSDKActivity.java
@@ -1,65 +1,52 @@
 package com.musixmatch.lyrics.example;
 
-import com.musixmatch.lyrics.musiXmatchLyricsConnector;
-import com.musixmatch.lyrics.example.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
+import com.musixmatch.lyrics.MissingPluginException;
+import com.musixmatch.lyrics.musiXmatchLyricsConnector;
 
 public class musiXmatchSDKActivity extends Activity {
-	private musiXmatchLyricsConnector lyricsPlugin= null;
-	
-    /** Called when the activity is first created. */
+    private musiXmatchLyricsConnector mLyricsPlugin = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        final String mArtistName = "The Beatles";
-        final String mTrackName  = "Let It Be";
-        lyricsPlugin = new musiXmatchLyricsConnector(this);
-        
-        // Please uncomment this line when approved and before going in production
-        //
-        // lyricsPlugin.setTestMode(false);
 
-        Button btn = (Button) findViewById(R.id.showLyrics);
-		btn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (lyricsPlugin.getIsBound())
-					try {
-						lyricsPlugin.startLyricsActivity(mArtistName,mTrackName);
-					} catch (RemoteException e) {
-						lyricsPlugin.downloadLyricsPlugin();
-					}
-				else
-					lyricsPlugin.downloadLyricsPlugin();
-			}
-		}); 
+        setContentView(R.layout.main);
+
+        final String artistName = "The Beatles";
+        final String trackName = "Let It Be";
+
+        mLyricsPlugin = new musiXmatchLyricsConnector(this);
+        mLyricsPlugin.setLoadingMessage("Your custom title", "Your custom message");
+
+        findViewById(R.id.showLyrics).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mLyricsPlugin.startLyricsActivity(artistName, trackName);
+                } catch (MissingPluginException e) {
+                    mLyricsPlugin.downloadLyricsPlugin();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-    
+
     @Override
     protected void onResume() {
-    	// TODO Auto-generated method stub
-    	if (!lyricsPlugin.getIsBound())
-    		lyricsPlugin.doBindService();
-
-    	super.onResume();
+        mLyricsPlugin.doBindService();
+        super.onResume();
     }
-    
+
     @Override
-    protected void onDestroy() {
-    	// TODO Auto-generated method stub
-    	if (lyricsPlugin.getIsBound())
-    		lyricsPlugin.doUnbindService();
-    	super.onDestroy();
+    protected void onPause() {
+        mLyricsPlugin.doUnbindService();
+        super.onPause();
     }
-    
-    
-
 }
